9bf1bb911b7dd972beaadf5fcea614213b9f528c
Arunjith <arunjithk987@Gmail.com>
2025-02-21 11:42:00 +0530

    login-finished

--- src/components/pages/Myprofile.jsx
+++ src/components/pages/Myprofile.jsx
@@ -1,9 +1,59 @@
-import 'react'
-
+import "react";
+import { useState } from "react";
+import { assets } from "../../assets/assets";
 const Myprofile = () => {
-  return (
-    <div>Myprofile</div>
-  )
-}
+  const [userData, setUserData] = useState({
+    name: "Edward Vincent",
+    image: assets.profile_pic,
+    email: "richardjames@gmail.com",
+    address: {
+      line1: "123 Main St",
+      line2: "Anytown, USA",
+    },
+    gender: "Male",
+    dob: "1990-01-01",
+  });
 
-export default Myprofile
+  const [isEdit, setIsEdit] = useState(true);
+
+  return (
+    <div>
+      <img src={userData.image} alt="" />
+
+      {isEdit ? (
+        <input
+          type="text"
+          value={userData.name}
+          onChange={(e) =>
+            setUserData((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+      ) : (
+        <p>{userData.name}</p>
+      )}
+
+      <hr />
+      <div>
+        <p>CONTACT INFORMATION</p>
+        <div>
+          <p>EMAIl ID:</p>
+          <p>{userData.email}</p>
+          <p>Phone:</p>
+          {isEdit ? (
+            <input
+              type="text"
+              value={userData.phone}
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, phone: e.target.value }))
+              }
+            />
+          ) : (
+            <p>{userData.phone}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Myprofile;
